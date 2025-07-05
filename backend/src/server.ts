7e9f3a6c7d303b7fbd8a80de7686b96819309586
BDz3us <lucas.emanuel@indwise.com>
2025-07-05 19:38:49 -0300

    fix env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
 import { createIcloudEvent } from "./createEnvent";
 import { getIcloudEvent } from "./getEvents";
